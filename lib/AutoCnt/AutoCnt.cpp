808f3db434914d5374bf42592553ea9a0bb2f874
pablomoreira <pablolokekeiran@gmail.com>
2022-04-07 18:29:03 -0300

    modified:   lib/AutoCnt/AutoCnt.cpp

--- lib/AutoCnt/AutoCnt.cpp
+++ lib/AutoCnt/AutoCnt.cpp
@@ -10,15 +10,15 @@ AutoCnt::AutoCnt(String ssid) {
 
   WiFi.mode(WIFI_OFF); //agrega a empezar con el wifi apagado, evitar estrellarse
   WiFi.mode(WIFI_AP);
-  this->_dnss = new DNSServer();
-  this->_server = new WiFiServer(80);
+  //this->_dnss = new DNSServer();
+  //this->_server = new WiFiServer(80);
   this->_apIP.fromString("10.10.10.1");
 
-  WiFi.softAP("DNSServer CaptivePortal example");
+  WiFi.softAP("xxx");
   WiFi.softAPConfig(this->_apIP, this->_apIP, IPAddress(255, 255, 255, 0));
 
-  this->_dnss->start(_DNS_PORT, "*", this->_apIP);
-  this->_server->begin();
+  //this->_dnss->start(_DNS_PORT, "*", this->_apIP);
+  //this->_server->begin();
 
 }
 
@@ -28,31 +28,7 @@ AutoCnt::~AutoCnt(void) {
 }
 
 void AutoCnt::loop(){
-while(1){
-    this->_dnss->processNextRequest();
-  WiFiClient client = this->_server->available();   // escuchar para los clientes entrantes
+    while(1){
 
-  if (client) {
-    String currentLine = "";
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        if (c == '\n') {
-          if (currentLine.length() == 0) {
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
-            //client.print(responseHTML);
-            break;
-          } else {
-            currentLine = "";
-          }
-        } else if (c != '\r') {
-          currentLine += c;
-        }
-      }
     }
-    client.stop();
-  }
-}
 }
